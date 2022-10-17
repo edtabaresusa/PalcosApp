15f6f633d71c2d076be3a4c45e264dbbb052e9f3
Âedtabaresusa <eduar.tabares.mt@usa.edu.co>
2022-10-16 20:49:48 -0500

    add reports

--- src/main/java/com/usa/palcosapp/repository/ReservationRepository.java
+++ src/main/java/com/usa/palcosapp/repository/ReservationRepository.java
@@ -1,11 +1,16 @@
 package com.usa.palcosapp.repository;
 
 import com.usa.palcosapp.model.Box;
+import com.usa.palcosapp.model.Client;
+import com.usa.palcosapp.model.ClientReport;
 import com.usa.palcosapp.model.Reservation;
 import com.usa.palcosapp.repository.crudRepository.ReservationCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +34,22 @@ public class ReservationRepository {
 
     public void delete(Reservation reservation){
         reservationCrudRepository.delete(reservation);
+    }
+
+    public List<Reservation> getReservationByStatus(String status){
+        return reservationCrudRepository.findAllByStatus(status);
+    }
+
+    public List<Reservation> getReservationPeriod(Date dateOne, Date dateTwo){
+        return reservationCrudRepository.findAllByStartDateAfterAndStartDateBefore(dateOne, dateTwo);
+    }
+
+    public List<ClientReport> getTopClients(){
+        List<ClientReport> res = new ArrayList<>();
+        List<Object[]> report = reservationCrudRepository.countTotalReservationsByClient();
+        for (int i = 0; i < report.size(); i++) {
+            res.add(new ClientReport((Long) report.get(i)[1], (Client) report.get(i)[0]));
+        }
+        return  res;
     }
 }
